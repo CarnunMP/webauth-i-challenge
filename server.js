923473ad97c6cf5f7010a9989d0152c25f830674
CarnunMP <carnun@hotmail.co.uk>
2019-11-12 15:11:05 +0000

    added sessions code to endpoints

--- server.js
+++ server.js
@@ -54,14 +54,15 @@ server.post ('/api/register', (req, res) => {
     });
 });
 
-let authed = false; // Massively imperfect solution, I know! :P
+// let authed = false; // Massively imperfect solution, I know! :P
 server.post('/api/login', (req, res) => {
   const { username, password } = req.body;
 
   Users.findBy({ username })
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        authed = true;
+        // authed = true;
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -73,7 +74,7 @@ server.post('/api/login', (req, res) => {
 });
 
 server.get('/api/users', (req, res) => {
-  if (authed) {
+  if (req.session && req.session.user) {
     Users.find()
     .then(users => {
       res.json(users);
